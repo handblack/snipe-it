489f881c002c1417193e2e335cc2a9403fa2c661
handblack <soporte@miasoftware.net>
2022-08-26 13:56:45 -0500

    add languaje es-PE

---
+++ resources/lang/es-PE/admin/depreciations/general.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'about_asset_depreciations'  			=> 'Info Depreciación Equipos',
+    'about_depreciations'  					=> 'Puedes configurar clases de depreciación para amortizar equipos de forma lineal.',
+    'asset_depreciations'  					=> 'Amortizaciones',
+    'create'  					            => 'Crear Depreciación',
+    'depreciation_name'  					=> 'Nombre Depreciación',
+    'depreciation_min'                      => 'Floor Value of Depreciation',
+    'number_of_months'  					=> 'Meses',
+    'update'  					            => 'Actualizar Depreciación',
+    'depreciation_min'                      => 'Valor mínimo después de depreciado',
+    'no_depreciations_warning'               => '<strong>Warning: </strong>
+						Actualmente no tiene ninguna depreciación configurada.
+						Configure al menos una depreciación para ver el informe de depreciación.',
+];
